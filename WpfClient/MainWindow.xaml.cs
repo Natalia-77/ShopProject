e53242368fecd5d7d7d2cc530cf15c513c353d66
Kvazar3S273 <ba2h007@gmail.com>
2021-06-09 21:47:43 +0300

    Create AdminWindow

--- WpfClient/MainWindow.xaml.cs
+++ WpfClient/MainWindow.xaml.cs
@@ -33,41 +33,66 @@ namespace WpfClient
         public MainWindow()
         {
             InitializeComponent();
+            var app = App.Current as IGetConfiguration;
+            var serverUrl = app.Configuration.GetSection("ServerUrl").Value;
 
-            Products = new ObservableCollection<Products>
+            using (WebClient client = new WebClient())
             {
-                new Products { 
-                    Name="Олівці",
-                    Description = "Різнокольорові олівці відомої фірми Марко (Чехія). Набір 24 кольори. Яскраві, приємні для сприйняття",
-                    Price=35, 
-                   // Image="D:\\ШАГ\\0 Repository\\ShopProject\\ShopProject\\Photos\\01.jpg"
-                    },
-                new Products { 
-                    Name="Набір",
-                    Description = "Набір канцелярського приладдя для школярів. Включає кольорові олівці, фломастери, фарби акварельні, гуаші, лінійки, клей",
-                    Price=305,
-                    //Image="D:\\ШАГ\\0 Repository\\ShopProject\\ShopProject\\Photos\\02.jpg"
-                   },
-                new Products { 
-                    Name="Офісне приладдя",
-                    Description = "Набір канцтоварів для офісу. До складу входять: олівці, ручки, підставка, блокноти, калькулятор, лінійка",
-                    Price=520,
-                    //Image="D:\\ШАГ\\0 Repository\\ShopProject\\ShopProject\\Photos\\03.jpg"
-                   },
-                new Products { 
-                    Name="Фломастери",
-                    Description = "Набір різнокольорових фломастерів чеської фірми Кох-і-нор. 36 фломастерів відмінної якості з екологічними барвниками",
-                    Price=75,
-                    //Image="D:\\ШАГ\\0 Repository\\ShopProject\\ShopProject\\Photos\\04.jpg"
-                    },
-                new Products { 
-                    Name="Палички",
-                    Description = "Палички для лічби. Призначені для учнів дошкільного та молодшого шкільного віку. 40 штук",
-                    Price=16,
-                    //Image="D:\\ШАГ\\0 Repository\\ShopProject\\ShopProject\\Photos\\05.jpg"
-                    }
-            };
-            productList.ItemsSource = Products;
+                client.DownloadDataCompleted += asyncWeb_DownloadDataCompleted;
+                Uri url = new Uri($"{serverUrl}Account/show");
+                client.DownloadDataAsync(url);
+            }
+
+            //Products = new ObservableCollection<Products>
+            //{
+            //    new Products { 
+            //        Name="Олівці",
+            //        Description = "Різнокольорові олівці відомої фірми Марко (Чехія). Набір 24 кольори. Яскраві, приємні для сприйняття",
+            //        Price=35, 
+            //       // Image="D:\\ШАГ\\0 Repository\\ShopProject\\ShopProject\\Photos\\01.jpg"
+            //        },
+            //    new Products { 
+            //        Name="Набір",
+            //        Description = "Набір канцелярського приладдя для школярів. Включає кольорові олівці, фломастери, фарби акварельні, гуаші, лінійки, клей",
+            //        Price=305,
+            //        //Image="D:\\ШАГ\\0 Repository\\ShopProject\\ShopProject\\Photos\\02.jpg"
+            //       },
+            //    new Products { 
+            //        Name="Офісне приладдя",
+            //        Description = "Набір канцтоварів для офісу. До складу входять: олівці, ручки, підставка, блокноти, калькулятор, лінійка",
+            //        Price=520,
+            //        //Image="D:\\ШАГ\\0 Repository\\ShopProject\\ShopProject\\Photos\\03.jpg"
+            //       },
+            //    new Products { 
+            //        Name="Фломастери",
+            //        Description = "Набір різнокольорових фломастерів чеської фірми Кох-і-нор. 36 фломастерів відмінної якості з екологічними барвниками",
+            //        Price=75,
+            //        //Image="D:\\ШАГ\\0 Repository\\ShopProject\\ShopProject\\Photos\\04.jpg"
+            //        },
+            //    new Products { 
+            //        Name="Палички",
+            //        Description = "Палички для лічби. Призначені для учнів дошкільного та молодшого шкільного віку. 40 штук",
+            //        Price=16,
+            //        //Image="D:\\ШАГ\\0 Repository\\ShopProject\\ShopProject\\Photos\\05.jpg"
+            //        }
+            //};
+            //productList.ItemsSource = Products;
+        }
+
+        public void asyncWeb_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
+        {
+            try
+            {
+                string res = Encoding.Default.GetString(e.Result);
+                var list = JsonConvert.DeserializeObject<List<Products>>(res);
+                productList.ItemsSource = list;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.GetBaseException().ToString());
+            }
+
         }
 
         private void productList_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -76,8 +101,8 @@ namespace WpfClient
             tbName.Text = prod.Name;
             tbPrice.Text = prod.Price.ToString();
             tbDescription.Text = prod.Description;
-           // Uri fileUri = new Uri(prod.Image);
-            //imgPhoto.Source = new BitmapImage(fileUri);
+            Uri fileUri = new Uri(prod.Image);
+            imgPhoto.Source = new BitmapImage(fileUri);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
